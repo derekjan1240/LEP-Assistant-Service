99717b26a7eb88b6c910afeb13895d944ba3e8b3
Kai-Jian Zhan (Derek) <derekjan1240@gmail.com>
2020-12-15 18:52:09 +0800

    add starting log

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8082);
+  await app.listen(8082, () => {
+    Logger.log('Assistant Service is running on port 8082!');
+  });
 }
 bootstrap();
